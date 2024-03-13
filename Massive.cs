8ed55612b906727bc762e3c55e23dfa440cd9e17
AdianCol <artem.baykin27@gmail.com>
2024-03-13 22:49:35 +0300

    3-4

---
+++ Massive.cs
@@ -0,0 +1,29 @@
+﻿public abstract class Massive<T> : IPrinter
+{
+    protected IProvider<T> _provider;
+
+
+    public abstract void CreateMassiveByRandom();
+
+    public abstract void CreateMassiveByUser();
+
+    public virtual void InitializeArray(bool choice)
+    {
+        if(choice)
+        {
+            CreateMassiveByUser();
+        }
+        else
+        {
+            CreateMassiveByRandom();
+        }
+    }
+
+    public abstract void Print();
+
+    public Massive(bool choice, IProvider<T> provider)
+    {
+        _provider = provider;
+        InitializeArray(choice);
+    }
+}
---
+++ Prog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Prog{
+    static void Main()
+    {
+        IProvider<int> intarrayprov = new  IntValueProvider();
+        IProvider<double> doubarrayprov = new  DoubleValueProvider();
+        IProvider<bool> boolarrayprov = new  BoolValueProvider();
+        IProvider<string> strarrayprov = new  StringValueProvider();
+
+        OneDemensionMassive<int> intArray1= new OneDemensionMassive<int>(intarrayprov);
+        OneDemensionMassive<double> doubleArray1 = new OneDemensionMassive<double>(doubarrayprov);
+        OneDemensionMassive<bool> boolArray1 = new OneDemensionMassive<bool>(boolarrayprov);
+        OneDemensionMassive<string> stringArray1 = new OneDemensionMassive<string>(strarrayprov);
+
+        TwoDemensionMassive<int> intArray2 = new TwoDemensionMassive<int>(intarrayprov);
+        TwoDemensionMassive<double> doubleArray2 = new TwoDemensionMassive<double>(doubarrayprov);
+        TwoDemensionMassive<bool> boolArray2 = new TwoDemensionMassive<bool>(boolarrayprov);
+        TwoDemensionMassive<string> stringArray2 = new TwoDemensionMassive<string>(strarrayprov);
+
+        IPrinter[] printers = new IPrinter[]
+        {
+            intArray1, doubleArray1, boolArray1, stringArray1, intArray2, doubleArray2, boolArray2, stringArray2
+        };
+
+        foreach (IPrinter printer in printers)
+        {
+            Console.WriteLine($"Элементы массива {printer.GetType()}:");
+            printer.Print();
+        }
+    }
+}
